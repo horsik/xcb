3caac4b8bd418bd97257abd521e9593a7274a485
horsik <kampaw@outlook.com>
2014-09-05 23:05:38 +0200

    xcb_get_setup, xcb_setup_roots_iterator

--- src/php_xcb.h
+++ src/php_xcb.h
@@ -14,8 +14,18 @@
 #define PHP_XCB_MODULE_NAME "XCB"
 #define PHP_XCB_VERSION "1.0"
 
-#define zend_register_list_destructors(s) \
-    le_##s = zend_register_list_destructors_ex(s##_dtor, NULL, #s, module_number)
+#define zend_register_list_destructors(s, dtor) \
+    le_##s = zend_register_list_destructors_ex(dtor, NULL, #s, module_number)
+
+#define property_long(name) add_property_long(dest, #name, src->name)
+#define property_bool(name) add_property_bool(dest, #name, src->name)
+#define property_string(name) add_property_string(dest, #name, src->name)
+
+#define assoc_long(name) add_assoc_long(dest, #name, src->name)
+#define assoc_bool(name) add_assoc_bool(dest, #name, src->name)
+#define assoc_string(name) add_assoc_string(dest, #name, src->name)
+
+#define STRUCT_TO_OBJECT(type) void type##_to_object(type *src, zval *dest)
 
 extern zend_module_entry xcb_module_entry;
 #define phpext_xcb_ptr &xcb_module_entry;
@@ -28,5 +38,7 @@ PHP_FUNCTION(xcb_disconnect);
 PHP_FUNCTION(xcb_connection_has_error);
 PHP_FUNCTION(xcb_flush);
 PHP_FUNCTION(xcb_generate_id);
+PHP_FUNCTION(xcb_get_setup);
+PHP_FUNCTION(xcb_setup_roots_iterator);
 
 #endif
--- src/xcb.c
+++ src/xcb.c
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2011 The PHP Group                                |
+  | Copyright (c) 1997-2014 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -24,6 +24,8 @@ zend_function_entry xcb_functions[] = {
     PHP_FE(xcb_connection_has_error, NULL)
     PHP_FE(xcb_flush, NULL)
     PHP_FE(xcb_generate_id, NULL)
+    PHP_FE(xcb_get_setup, NULL)
+    PHP_FE(xcb_setup_roots_iterator, NULL)
     {NULL, NULL, NULL}
 };
 
@@ -45,9 +47,31 @@ ZEND_GET_MODULE(xcb)
 #endif
 
 xcb_connection_t *c = NULL;
+static int le_xcb_setup;
+
+STRUCT_TO_OBJECT(xcb_screen_t)
+{
+    property_long(root);
+    property_long(default_colormap);
+    property_long(white_pixel);
+    property_long(black_pixel);
+    property_long(current_input_masks);
+    property_long(width_in_pixels);
+    property_long(height_in_pixels);
+    property_long(width_in_millimeters);
+    property_long(height_in_millimeters);
+    property_long(min_installed_maps);
+    property_long(max_installed_maps);
+    property_long(root_visual);
+    property_long(backing_stores);
+    property_long(save_unders);
+    property_long(root_depth);
+    property_long(allowed_depths_len);
+}
 
 PHP_MINIT_FUNCTION(xcb)
 {
+    zend_register_list_destructors(xcb_setup, NULL);
     return SUCCESS;
 }
 
@@ -97,4 +121,36 @@ PHP_FUNCTION(xcb_flush)
 PHP_FUNCTION(xcb_generate_id)
 {
     RETURN_LONG(xcb_generate_id(c));
+}
+
+PHP_FUNCTION(xcb_get_setup)
+{
+    const xcb_setup_t *setup = xcb_get_setup(c);
+    ZEND_REGISTER_RESOURCE(return_value, (xcb_setup_t *) setup, le_xcb_setup);
+}
+
+PHP_FUNCTION(xcb_setup_roots_iterator)
+{
+    zval *rsrc, *data;
+    const xcb_setup_t *setup;
+    xcb_screen_iterator_t iterator;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+                              "r", &rsrc) == FAILURE) {
+        return;
+    }
+
+    ZEND_FETCH_RESOURCE(setup, xcb_setup_t*, &rsrc, -1, "xcb_setup_t",
+                        le_xcb_setup);
+
+    object_init(return_value);
+    ALLOC_INIT_ZVAL(data);
+    object_init(data);
+
+    iterator = xcb_setup_roots_iterator(setup);
+    xcb_screen_t_to_object(iterator.data, data);
+
+    add_property_zval(return_value, "data", data);
+    add_property_long(return_value, "rem", iterator.rem);
+    add_property_long(return_value, "index", iterator.index);
 }
